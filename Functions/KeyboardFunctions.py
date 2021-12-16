061453dcc793625ac78bef56bc848946c9e6c30b
Hossein <hossein73.z@gmail.com>
2021-12-16 15:16:04 +0330

    Improved performance by reducing usage of read function

--- Functions/KeyboardFunctions.py
+++ Functions/KeyboardFunctions.py
@@ -6,10 +6,20 @@ from Objects.RawSpecialButton import RawSpecialButton
 
 
 def get_button_array_array(person: Person, button_id):
-    raw_buttons: list[RawButton] = read(table=raw_buttons_table, id=button_id, my_object=RawButton)
+    raw_buttons: list[RawButton] = read(table=raw_buttons_table, my_object=RawButton)
+    raw_special_buttons: list[RawSpecialButton] = read(table=raw_special_buttons_table, my_object=RawSpecialButton)
 
+    buttons_dict = {}
     if raw_buttons is not None:
-        raw_button = raw_buttons[0]
+        for button in raw_buttons:
+            buttons_dict[button.button_id] = button
+    special_buttons_dict = {}
+    if raw_special_buttons is not None:
+        for button in raw_special_buttons:
+            special_buttons_dict[button.special_button_id] = button
+
+    try:
+        raw_button = buttons_dict[button_id]
         button_id_array_array = raw_button.button_keyboards
         special_button_id_array_array = raw_button.button_special_keyboards
 
@@ -20,7 +30,7 @@ def get_button_array_array(person: Person, button_id):
                 button_array: list[KeyboardButton] = []
                 for button_id in button_id_array:
                     try:
-                        temp_button: RawButton = read(table=raw_buttons_table, id=button_id, my_object=RawButton)[0]
+                        temp_button: RawButton = buttons_dict[button_id]
                         if temp_button.button_admin_key and person.person_is_admin or not temp_button.button_admin_key:
                             button_array.append(KeyboardButton(text=temp_button.button_text))
                     except Exception as e:
@@ -33,8 +43,7 @@ def get_button_array_array(person: Person, button_id):
                 special_button_array = []
                 for special_button_id in special_button_id_array:
                     try:
-                        temp_button: RawSpecialButton = read(
-                            table=raw_special_buttons_table, id=special_button_id, my_object=RawSpecialButton)[0]
+                        temp_button: RawSpecialButton = special_buttons_dict[special_button_id]
                         if temp_button.special_button_admin_key and \
                                 person.person_is_admin or not temp_button.special_button_admin_key:
                             special_button_array.append(KeyboardButton(text=temp_button.special_button_text))
@@ -43,41 +52,47 @@ def get_button_array_array(person: Person, button_id):
                         break
                 button_array_array.append(special_button_array)
         return button_array_array
+    except Exception as e:
+        print(e)
+        return [[]]
 
 
 def get_pressed_button(person: Person, text: str):
+    raw_buttons: list[RawButton] = read(table=raw_buttons_table, my_object=RawButton)
+    raw_special_buttons: list[RawSpecialButton] = read(table=raw_special_buttons_table, my_object=RawSpecialButton)
+
+    buttons_dict = {}
+    if raw_buttons is not None:
+        for button in raw_buttons:
+            buttons_dict[button.button_id] = button
+    special_buttons_dict = {}
+    if raw_special_buttons is not None:
+        for button in raw_special_buttons:
+            special_buttons_dict[button.special_button_id] = button
+
     result = None
     try:
-        raw_buttons: list[RawButton] = read(
-            table=raw_buttons_table, my_object=RawButton, id=person.person_last_button_id)
-        if raw_buttons is not None:
-            raw_button = raw_buttons[0]
-            button_id_array_array = raw_button.button_keyboards
-            for button_id_array in button_id_array_array:
-                for button_id in button_id_array:
-                    buttons = read(table=raw_buttons_table, my_object=RawButton, id=button_id)
-                    if buttons is not None:
-                        button = buttons[0]
-                        if (button.button_text == text) \
-                                and ((raw_button.button_admin_key and person.person_is_admin)
-                                     or not raw_button.button_admin_key):
-                            result = {'pressed_button': button, 'is_special': False}
-                            break
+        raw_button = buttons_dict[person.person_last_button_id]
+        button_id_array_array = raw_button.button_keyboards
+        for button_id_array in button_id_array_array:
+            for button_id in button_id_array:
+                button = buttons_dict[button_id]
+                if (button.button_text == text) \
+                        and ((raw_button.button_admin_key and person.person_is_admin)
+                             or not raw_button.button_admin_key):
+                    result = {'pressed_button': button, 'is_special': False}
+                    break
 
     except Exception as e:
         print(Fore.RED + str(e) + Fore.RESET)
 
     if result is None:
         try:
-            special_button_id_array_array = read(
-                table=raw_buttons_table,
-                id=person.person_last_button_id,
-                my_object=RawButton)[0].button_special_keyboards
+            special_button_id_array_array = buttons_dict[person.person_last_button_id].button_special_keyboards
 
             for special_button_id_array in special_button_id_array_array:
                 for special_button_id in special_button_id_array:
-                    special_button = read(
-                        table=raw_special_buttons_table, id=special_button_id, my_object=RawSpecialButton)[0]
+                    special_button = special_buttons_dict[special_button_id]
 
                     if (special_button.special_button_text == text) \
                             and ((special_button.special_button_admin_key and person.person_is_admin)
