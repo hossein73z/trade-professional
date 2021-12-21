8b52044c47e63e9158b78d0b79a5af2f0b950c76
Hossein <hossein73.z@gmail.com>
2021-12-21 14:33:40 +0330

    Add limit order confirmation level completed

--- Functions/AddOrder.py
+++ Functions/AddOrder.py
@@ -346,49 +346,25 @@ def add_order_type(person: Person, update: Update, context: CallbackContext):
     if update.effective_message.text == 'بازار' or update.effective_message.text == 'محدود':
         if update.effective_message.text == 'محدود':
 
-            mssg = 'لطفا چند لحظه صبر کنید!'
+            progress['stage'] = 'AddOrder_price'
+            progress['value']['type'] = Client.ORDER_LIMIT
+            person.person_progress = json.dumps(progress)
+            update_person(person)
+
+            mssg = 'در چه قیمتی میخواهید سفارش را قرار دهید؟ '
+            # Create keyboard to be sent
+            button_array_array = get_button_array_array(person, person.person_last_button_id)
+            reply_markup = ReplyKeyboardMarkup(
+                keyboard=button_array_array, resize_keyboard=True, one_time_keyboard=False, selective=False)
             try:
-                message: Message = context.bot.sendMessage(
-                    chat_id=person.person_chat_id, text=mssg, reply_markup=ReplyKeyboardRemove())
+                context.bot.sendMessage(chat_id=person.person_chat_id, text=mssg, reply_markup=reply_markup)
             except Exception as e:
                 print(e)
-                message: Message = context.bot.sendMessage(
-                    chat_id=person.person_chat_id, text=str(e), reply_markup=ReplyKeyboardRemove)
-
-            exchange = read(
-                exchanges_table, Exchange, person_id=person.person_id, name=progress['value']['exchange'])[0]
-            if exchange is not None:
-
-                client = Client(
-                    api_key=exchange.api_key,
-                    api_secret=exchange.api_secret,
-                    passphrase=exchange.api_passphrase,
-                    sandbox=exchange.api_sandbox)
-                ticker = client.get_ticker(f"{progress['value']['currency']}-{progress['value']['base']}")
-
-                if ticker is not None:
-                    progress['stage'] = 'AddOrder_price'
-                    progress['value']['type'] = Client.ORDER_LIMIT
-                    person.person_progress = json.dumps(progress)
-                    update_person(person)
-
-                    mssg = f'در چه قیمتی میخواهید سفارش را قرار دهید؟ ' \
-                           f'قیمت جفت ارز مورد نظر در این لحظه برابر {ticker["price"]} میباشد'
-                    # Create keyboard to be sent
-                    button_array_array = get_button_array_array(person, person.person_last_button_id)
-                    reply_markup = ReplyKeyboardMarkup(
-                        keyboard=button_array_array, resize_keyboard=True, one_time_keyboard=False, selective=False)
-                    try:
-                        context.bot.sendMessage(chat_id=person.person_chat_id, text=mssg, reply_markup=reply_markup)
-                        context.bot.deleteMessage(chat_id=person.person_chat_id, message_id=message.message_id)
-                    except Exception as e:
-                        print(e)
-                        context.bot.sendMessage(chat_id=person.person_chat_id, text=str(e), reply_markup=reply_markup)
+                context.bot.sendMessage(chat_id=person.person_chat_id, text=str(e), reply_markup=reply_markup)
 
         elif update.effective_message.text == 'بازار':
             progress['value']['type'] = Client.ORDER_MARKET
             # progress['stage'] = 'AddOrder_value'
-
 
     else:
         mssg = 'پیام مفهوم نبود. لطفا یکی از گیزنه های زیر را انتخاب کنید'
@@ -433,7 +409,7 @@ def add_order_price(person: Person, update: Update, context: CallbackContext):
             mssg = 'چه مقدار %s میخواهید بفروشید؟ ' \
                    'لطفا مقدار %s را برای فروش ارسال کنید. ' \
                    'دقت کنید که مقدار خود ارزی که میخواهید بفروشید را ارسال کنید، ' \
-                   'نه مقدار دلار یا ارز پایه. برای مثال برای فروش 0.2 %s در قیمت وارد شده که معادل %s% s میشود، ' \
+                   'نه مقدار دلار یا ارز پایه. برای مثال برای فروش 0.2 %s در قیمت وارد شده که معادل %s %s میشود، ' \
                    'تنها عدد 0.2 را ارسال کنید' \
                    % (currency, currency, currency, str(price * 0.2), base)
 
@@ -441,7 +417,7 @@ def add_order_price(person: Person, update: Update, context: CallbackContext):
             mssg = 'به چه اندازه %s میخواهید بخرید؟ ' \
                    'لطفا مقدار %s را برای خرید ارسال کنید. ' \
                    'دقت کنید که مقدار ارز پایه را برای خرید وارد کنید، ' \
-                   'نه مقدار %s. برای مثال برای خرید 0.2 %s که معادل %s% s میشود، تنها عدد %s را وارد کنید ' \
+                   'نه مقدار %s. برای مثال برای خرید 0.2 %s که معادل %s %s میشود، تنها عدد %s را وارد کنید ' \
                    % (currency, base, currency, currency, str(price * 0.2), base, str(price * 0.2))
         else:
             mssg = 'خطای ناشناخته پیش آمده. لطافا کلید لغو را زده و از ابتدا شروع کنید'
@@ -470,4 +446,104 @@ def add_order_price(person: Person, update: Update, context: CallbackContext):
 
 
 def add_order_value(person: Person, update: Update, context: CallbackContext):
-    pass
+    # Initialize Progress Stage
+    try:
+        progress = json.loads(person.person_progress)
+    except Exception as e:
+        progress = None
+        print('Person Progress To JSON Error: ', e)
+
+    text = update.effective_message.text
+    text = text.replace(" ", "")
+    text = FormatText.english_number(text=text)
+
+    value = 0.0
+    try:
+        value = float(text)
+    except Exception as e:
+        print(e)
+
+    if value > 0:
+        progress['stage'] = 'AddOrder_confirmation'
+        progress['value']['value'] = value
+        person.person_progress = json.dumps(progress)
+        update_person(person)
+        currency = progress['value']['currency']
+        base = progress['value']['base']
+        price = progress['value']['price']
+        exchanges: list[Exchange] = read(
+            table=exchanges_table, my_object=Exchange, person_id=person.person_id, name=progress['value']['exchange'])
+        exchange = exchanges[0]
+        client = Client(
+            api_key=exchange.api_key,
+            api_secret=exchange.api_secret,
+            passphrase=exchange.api_passphrase,
+            sandbox=exchange.api_sandbox)
+        ticker = client.get_ticker(f'{currency}-{base}')
+        accounts_currency = client.get_accounts(currency=currency)
+        accounts_base = client.get_accounts(currency=base)
+        market_percent = round(abs(float(ticker['price']) - price) * 100 / float(ticker['price']), 2)
+        value_amount = 0.0
+        amount = round(value * price, 8)
+        up_or_down = 'پایین تر' if float(ticker['price']) - price > 0 else 'بالاتر'
+        order_side = ''
+        value_name = ''
+        amount_name = ''
+
+        if progress['value']['side'] == Client.SIDE_SELL:
+            order_side = 'فروش'
+            value_name = currency
+            amount_name = base
+            for account in accounts_currency:
+                value_amount += float(account['balance'])
+
+        elif progress['value']['side'] == Client.SIDE_BUY:
+            order_side = 'خرید'
+            value_name = base
+            amount_name = currency
+            for account in accounts_base:
+                value_amount += float(account['balance'])
+
+        order_type = 'بازار' if progress['value']['type'] == Client.ORDER_MARKET else 'محدود'
+        portfolio_percent = round(
+            (value /
+             value_amount)
+            * 100,
+            2)
+
+        mssg = f"*خلاصه سفارش:*" + '\n' + \
+               f"*   نوع سفارش: *{order_side} {order_type}" + '\n' + \
+               f"*   صرافی: *{exchange.name}" + '\n' + \
+               f"*   جفت ارز: *{currency + '/' + base}" + '\n' + \
+               f"*   قیمت: *{str(price)}" + '\n' + \
+               f"*   مقدار: *{str(value)} {value_name}" + '\n' + \
+               f"ـ------------------------------" + '\n' + \
+               f"قیمت لحظه ای این جفت ارز برابر {str(ticker['price'])} میباشد." + '\n' + \
+               f"شما قصد دارید در {str(market_percent)} درصد {up_or_down} از قیمت بازار سفارش را قرار دهید." + '\n' + \
+               f"در این سفارش {str(portfolio_percent)}  درصد کل {value_name} های شما " + \
+               f"به ارزش {str(amount)} {amount_name} قرار خواهند گرفت." + '\n' + '\n' + \
+               f"در صورت تاییدو لطفا کلید تایید را فشار دهید"
+
+        # Create keyboard to be sent
+        button_array_array = [[KeyboardButton(text='تأیید ✅'), KeyboardButton(text='لغو ❌')]]
+        reply_markup = ReplyKeyboardMarkup(
+            button_array_array, resize_keyboard=True, one_time_keyboard=False, selective=False)
+        try:
+            context.bot.sendMessage(
+                chat_id=person.person_chat_id, text=mssg, reply_markup=reply_markup, parse_mode='Markdown')
+        except Exception as e:
+            print(e)
+            context.bot.sendMessage(
+                chat_id=person.person_chat_id, text=str(e), reply_markup=reply_markup, parse_mode='Markdown')
+
+    else:
+        mssg = "مقدار وارد شده صحیح نیست. لطفا فقط عدد ارسال کنید"
+        # Create keyboard to be sent
+        button_array_array = get_button_array_array(person, person.person_last_button_id)
+        reply_markup = ReplyKeyboardMarkup(
+            keyboard=button_array_array, resize_keyboard=True, one_time_keyboard=False, selective=False)
+        try:
+            context.bot.sendMessage(chat_id=person.person_chat_id, text=mssg, reply_markup=reply_markup)
+        except Exception as e:
+            print(e)
+            context.bot.sendMessage(chat_id=person.person_chat_id, text=str(e), reply_markup=reply_markup)
